fa8266b6850617a0fa7445134fbd41c18c12e289
Amit01999 <amitsarkar019999@gmail.com>
2025-03-25 23:49:19 +0600

    remove redirect

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Important for deployment
-  build: {
-    outDir: 'dist', // Ensure this is your Render "Publish Directory"
-  },
 });
